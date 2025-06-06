99a7d09cb45b24be5ad0e47a25aa69858215e5fc
Rmin-cmd <Rminpishehvar1379@gmail.com>
2025-06-06 12:28:45 +0330

    init

--- extract_pretrainFeat.py
+++ extract_pretrainFeat.py
@@ -99,8 +99,11 @@ if __name__ == '__main__':
                 args.dataset, args.randSeed, label_type))
 
         else:
-            save_dir = os.path.join(data_dir, r'raw_28video_batch28_dataset_%s_timeLen%d_tf16_sf16_multiFact2_lr0.000700_wd0.015000_epochs80_randSeed7_fold10_%s_c0.100000_l1.000000' % (
-                args.dataset, args.timeLen, label_type))
+            # save_dir = os.path.join(data_dir, r'raw_28video_batch28_dataset_%s_timeLen%d_tf16_sf16_multiFact2_lr0.000700_wd0.015000_epochs80_randSeed7_fold10_%s_c0.100000_l1.000000' % (
+            #     args.dataset, args.timeLen, label_type))
+
+            save_dir = os.path.join(data_dir,
+                                    r'raw_28video_batch28_dataset_both_timelen5_tf16_sf16_multiFact2_lr0.000700_wd0.015000_epochs80_randseed7_fold10_accSel_newPre_cls9')
 
     print(save_dir)
 
